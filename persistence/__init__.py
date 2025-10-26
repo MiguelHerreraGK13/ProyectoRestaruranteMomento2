21fa32fed0e3bd39b285c47e80afa13ebacde38e
miguel herrera <miguelherrera.gk1@gmail.com>
2025-10-26 09:31:08 -0500

    se depura desarrollo y volvemos a empezar de nuevo

--- persistence/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Inicializador de módulos para el paquete persistence
-para la importancion de conexiones, categorias y productos
-"""
-
-from .Conexion import Conexion
-from .Categoria_Repositorio import Categoria_Repositorio
-from .Producto_Repositorio import Producto_Repositorio
