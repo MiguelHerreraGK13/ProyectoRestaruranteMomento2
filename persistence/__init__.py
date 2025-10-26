d887ca65fb946dfc900e818f8b0b6700613a3c8c
miguel herrera <miguelherrera.gk1@gmail.com>
2025-10-26 11:57:49 -0500

    se agrega carpeta de persistencia y sus componentes

---
+++ persistence/__init__.py
@@ -0,0 +1,7 @@
+"""
+Inicializador de módulos para el paquete persistence
+"""
+
+from .Conexion import Conexion
+from .Categoria_Repositorio import Categoria_Repositorio
+from .Producto_Repositorio import Producto_Repositorio
